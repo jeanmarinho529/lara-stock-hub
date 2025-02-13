c216731594a5b21292fbd7a9742f4c67a56ba89e
jeanmarinho529 <jeanmarinho529@gmail.com>
2025-02-13 12:20:09 -0300

    fix: migrant adjustments

--- database/migrations/2025_02_10_234133_create_product_table.php
+++ database/migrations/2025_02_10_234133_create_product_table.php
@@ -13,11 +13,6 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            // adicionar tipo de produto
-            // acessorio, produto quimico,
-            // 
-            // 
-            // 
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('store_id');
             $table->string('name');
