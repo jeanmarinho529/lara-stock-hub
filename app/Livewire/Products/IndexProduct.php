a26977badbb5af7667ab9b620a8078bc0bc8dd46
jeanmarinho529 <jeanmarinho529@gmail.com>
2025-02-19 20:59:39 -0300

    feat: improve product index screen

--- app/Livewire/Products/IndexProduct.php
+++ app/Livewire/Products/IndexProduct.php
@@ -3,20 +3,50 @@
 namespace App\Livewire\Products;
 
 use App\Models\Product;
-
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class IndexProduct extends Component
 {
-    public $products;
+    public User $user;
+
+    public string $filter = '';
+
+    public string $type = '';
+
+    protected $queryString = ['filter', 'type'];
 
     public function mount()
     {
-        $this->products = Product::all();
+        $this->user = Auth::user();
     }
 
     public function render()
     {
-        return view('livewire.products.index-product');
+        $products = Product::select(
+            'id',
+            'brand_id',
+            'name',
+            'code',
+            'amount',
+            'type'
+        )
+            ->when($this->type, function ($query) {
+                return $query->where('type', $this->type);
+            })
+            ->when($this->filter, function ($query) {
+                return $query->where('name', 'like', "%$this->filter%")
+                    ->orWhere('code', 'like', "%$this->filter%");
+            })
+            ->with('brand:id,name')
+            ->withSum('productTransactions', 'quantity')
+            ->where('store_id', $this->user->store_id)
+            ->orderBy('id', 'desc')
+            ->paginate(10);
+
+        return view('livewire.products.index-product', [
+            'products' => $products,
+        ]);
     }
 }
--- resources/views/livewire/products/index-product.blade.php
+++ resources/views/livewire/products/index-product.blade.php
@@ -2,39 +2,27 @@
 @section('page', 'Todos os Produtos')
 
 <div>
-
-@if (session()->has('message'))
-            <div class="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md"
-                role="alert">
-                <div class="flex">
-                    <div class="py-1"><svg class="fill-current h-6 w-6 text-teal-500 mr-4"
-                            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                            <path
-                                d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z" />
-                        </svg></div>
-                    <div>
-                        <p class="font-bold">Sucesso</p>
-                        <p class="text-sm">{{ session('message') }}</p>
-                    </div>
-                </div>
-            </div>
-        @endif
-
-
-
+    <x-alert></x-alert>
 
     <div class="sm:col-span-6">
         <div class="overflow-x-auto">
 
             <div class="mb-10 grid grid-cols-1 sm:grid-cols-9 gap-x-6 gap-y-8">
-                <div class="sm:col-span-7">
-                    <x-input name="filter" wire:model.live="filter"
-                        placeholder="Filtre por Nome, Código, Tipo" autocomplete="off">Filtro</x-input>
+                <div class="sm:col-span-6">
+                    <x-input name="filter" wire:model.live="filter" placeholder="Filtre por Nome e Código"
+                        autocomplete="off">Filtro</x-input>
                 </div>
 
+                <div class="sm:col-span-2">
+                    <x-select name="type" label="Tipo do Produto" wire:model.live="type">
+                        <option value="" selected>Filtre por Tipo de Produto</option>
+                        <option value="product">Produto</option>
+                        <option value="service">Seriviço</option>
+                    </x-select>
+                </div>
 
                 <div class="sm:mt-9 sm:col-span-1">
-                    <a href="{{ route('products.create')}}"
+                    <a href="{{ route('products.create') }}"
                         class="rounded-md bg-indigo-600 px-3 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                         Novo Produto
                     </a>
@@ -48,18 +36,21 @@
                         <th class="px-4 py-2 text-left text-sm font-medium text-gray-600">Nome</th>
                         <th class="px-4 py-2 text-left text-sm font-medium text-gray-600">Marca</th>
                         <th class="px-4 py-2 text-left text-sm font-medium text-gray-600">Código</th>
+                        <th class="px-4 py-2 text-left text-sm font-medium text-gray-600">Total no Estoque</th>
                         <th class="px-4 py-2 text-left text-sm font-medium text-gray-600">Valor</th>
                         <th class="px-4 py-2 text-left text-sm font-medium text-gray-600"></th>
                     </tr>
                 </thead>
                 <tbody class="bg-white">
-                    @foreach ($products as $products)
+                    @foreach ($products as $product)
                         <tr class="border-b">
-                            <td class="px-4 py-2 text-sm text-gray-700">{{ $products['type'] }}</td>
-                            <td class="px-4 py-2 text-sm text-gray-700">{{ $products['name'] }}</td>
-                            <td class="px-4 py-2 text-sm text-gray-700">{{ $products->brand->name ?? 'Sem Marca' }}</td>
-                            <td class="px-4 py-2 text-sm text-gray-700">{{ $products['code'] }}</td>
-                            <td class="px-4 py-2 text-sm text-gray-700">R$ {{ $products['amount'] }}</td>
+                            <td class="px-4 py-2 text-sm text-gray-700">{{ $product['display_type'] }}</td>
+                            <td class="px-4 py-2 text-sm text-gray-700">{{ $product['name'] }}</td>
+                            <td class="px-4 py-2 text-sm text-gray-700">{{ $product['brand']['name'] ?? '' }}</td>
+                            <td class="px-4 py-2 text-sm text-gray-700">{{ $product['code'] }}</td>
+                            <td class="px-4 py-2 text-sm text-gray-700">
+                                {{ $product['product_transactions_sum_quantity'] ?? 0 }}</td>
+                            <td class="px-4 py-2 text-sm text-gray-700">R$ {{ $product['amount'] }}</td>
                             <td class="px-4 py-2 text-sm text-gray-700"></td>
                         </tr>
                     @endforeach
@@ -68,5 +59,8 @@
         </div>
     </div>
 
+    <div class="pagination mt-6">
+        {{ $products->links() }}
+    </div>
 
 </div>
