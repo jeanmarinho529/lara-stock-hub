b8890cbda6498d54f1e8a782d7a81441f1d230db
jeanmarinho529 <jeanmarinho529@gmail.com>
2025-02-19 20:00:10 -0300

    feat: improvements to the new products screen

--- app/Livewire/Products/CreateProduct.php
+++ app/Livewire/Products/CreateProduct.php
@@ -4,12 +4,19 @@ namespace App\Livewire\Products;
 
 use App\Models\Brand;
 use App\Models\Product;
+use App\Models\User;
+use Illuminate\Database\QueryException;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class CreateProduct extends Component
 {
+    public User $user;
+
+    #[Validate('string|nullable|required_if:type,product')]
+    public ?string $brand_id = null;
+
     #[Validate('required|string|min:4')]
     public string $name;
 
@@ -20,46 +27,59 @@ class CreateProduct extends Component
     public float $amount;
 
     #[Validate('required|integer|min:0')]
-    public int $minimun_quantity = 1;
+    public int $minimum_quantity = 1;
 
-    #[Validate('nullable|string|min:1')]
-    public string $description;
+    #[Validate('required|string|in:unit,centimeter,meter,gram,kilogram,liter,milliliter')]
+    public string $unit_measurement = 'unit';
 
     #[Validate('required|string|in:product,service')]
-    public string $type = 'product';
-    
-    #[Validate('string|required_if:type,product')]
-    public ?string $brand_id = '';
+    public string $type = '';
 
+    #[Validate('nullable|string|min:4')]
+    public string $description = '';
+
+    public function mount()
+    {
+        $this->user = Auth::user();
+    }
 
     public function save()
     {
         $this->validate();
 
-        $products = Product::firstOrCreate(
-            [
-                'user_id'  => Auth::user()->id,
-                'store_id' => Auth::user()->store_id,
-                'brand_id' => $this->brand_id,
-                'code'     => $this->code,
-                'name'     => $this->name,
-                'amount'   => $this->amount,
-            ],
-        );
+        try {
+            Product::create([
+                'brand_id'         => $this->brand_id,
+                'store_id'         => $this->user->store_id,
+                'user_id'          => $this->user->id,
+                'name'             => $this->name,
+                'code'             => $this->code,
+                'type'             => $this->type,
+                'amount'           => $this->amount,
+                'minimum_quantity' => $this->minimum_quantity,
+                'unit_measurement' => $this->unit_measurement,
+                'description'      => $this->description,
+            ]);
+        } catch (QueryException $e) {
+            if ($e->getCode() == 23000) {
+                session()->flash('waring', 'Código já cadastrado em outro produto, insira outro código!');
 
-        session()->flash('message', 'Produto "' . $this->name . '" criada com sucesso!');
+                return;
+            }
+
+            throw $e;
+        }
+
+        session()->flash('success', 'Produto cadastrado com sucesso.');
         redirect()->route('products.index');
     }
 
     public function render()
     {
-        {
-            $brands = Brand::where('store_id', Auth::user()->store_id)->get()->toArray();
-            
+        $brands = Brand::where('store_id', $this->user->store_id)->get()->toArray();
 
-            return view('livewire.products.create-product', [
-                'brands' => $brands,
-            ]);
-        }
+        return view('livewire.products.create-product', [
+            'brands' => $brands,
+        ]);
     }
 }
--- resources/views/livewire/products/create-product.blade.php
+++ resources/views/livewire/products/create-product.blade.php
@@ -2,13 +2,14 @@
 @section('page', 'Novo Produto')
 
 <div>
-    <form wire:submit="save" autocomplete="off">
+    <x-alert></x-alert>
 
+    <form wire:submit="save" autocomplete="off">
         <div class="space-y-12">
 
             <div class="border-b border-gray-900/10 pb-12">
                 <h2 class="text-base/7 font-semibold text-gray-900">Novo Produto</h2>
-                <p class="text-sm/6 text-gray-600">Insira os dados do novo Produto.</p>
+                <p class="text-sm/6 text-gray-600">Insira os dados do novo produto.</p>
 
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
 
@@ -17,61 +18,62 @@
                     </div>
 
                     <div class="sm:col-span-2">
-                        <x-input is_required name="code" wire:model="code"
-                            x-mask:dynamic="documentMask">Código</x-input>
+                        <x-input is_required name="code" wire:model="code">Código</x-input>
                     </div>
 
                     <div class="sm:col-span-2">
-                        <x-input is_required name="amount" wire:model="amount"
-                            x-mask:dynamic="documentMask">Valor</x-input>
+                        <x-input is_required type="number" min="0" name="amount" step="0.01"
+                            wire:model="amount">Valor</x-input>
                     </div>
 
                     <div class="sm:col-span-2">
-                        <x-input is_required name="minimun_quantity" wire:model="minimun_quantity"
-                            x-mask:dynamic="documentMask">Quantidade</x-input>
+                        <x-select name="type" wire:model="type" is_required label="Tipo">
+                            <option value="" disabled selected>Selecione o Tipo</option>
+                            <option value="product">Produto</option>
+                            <option value="service">Seriviço</option>
+                        </x-select>
                     </div>
 
                     <div class="sm:col-span-2">
-                        <x-input is_required name="description" wire:model="description"
-                            x-mask:dynamic="numberMask">Descrição</x-input>
+                        <x-input is_required type="number" min="0" name="minimum_quantity"
+                            wire:model="minimum_quantity">Quantidade Mínima de Produtos</x-input>
                     </div>
 
                     <div class="sm:col-span-2">
-                        <label for="type" class="block text-sm/6 font-medium text-gray-900">Tipo*</label>
-                        <div class="mt-2 grid grid-cols-1">
-                            <select required wire:model="type" id="type" name="type"
-                                class="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pr-8 pl-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
-                                <option value="" disabled selected>Selecione o Tipo</option>
-                                <option value="product">Produto</option>
-                                <option value="service">Seriviço</option>
-                            </select>
-                            @error('type')
-                                <span class="error text-red-600 sm:text-sm/6">{{ $message }}</span>
-                            @enderror
-                        </div>
+                        <x-select name="unit_measurement" wire:model="unit_measurement" is_required label="Unidade de Medida">
+                            <option value="" disabled selected>Selecione a Unidade de Medida</option>
+                            <option value="unit">UN - Unidade</option>
+                            <option value="meter">M - Metro</option>
+                            <option value="centimeter">CM - Centímetro</option>
+                            <option value="kilogram">KG - Kilograma</option>
+                            <option value="gram">G - Grama</option>
+                            <option value="liter">L - Litro</option>
+                            <option value="milliliter">ML - Mililitro</option>
+                        </x-select>
                     </div>
 
+                    <div class="sm:col-span-6">
+                        <x-select name="brand_id" wire:model="brand_id" is_required label="Marca do Produto">
+                            <option value="null" disabled selected>Selecione a Marca</option>
+                            @foreach ($brands as $brand)
+                                <option value="{{ $brand['id'] }}">{{ $brand['name'] }}</option>
+                            @endforeach
+                        </x-select>
+                    </div>
 
+                    <div class="sm:col-span-6">
+                        <label for="description" class="block text-sm/6 font-medium text-gray-900">
+                            Descrição
+                        </label>
 
-                    <div class="sm:col-span-2">
-                        <label for="brand_id" class="block text-sm/6 font-medium text-gray-900">Marca*</label>
-                        <div class="mt-2 grid grid-cols-1">
-                            <select wire:model="brand_id" id="brand_id" name="brand_id"
-                                class="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pr-8 pl-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
-                                <option value="" disabled selected>Selecione a Marca</option>
-                                @foreach ($brands as $brand)
-                                    <option value="{{ $brand['id'] }}">{{ $brand['name'] }}</option>
-                                @endforeach
-                            </select>
-                            @error('brand_id')
-                                <span class="error text-red-600 sm:text-sm/6">{{ $message }}</span>
-                            @enderror
-                        </div>
-
-                        <div class="sm:col-span-2">
-                            <div class="mt-2 grid grid-cols-1">
-                            </div>
-                        </div>
+                        <textarea id="description" name="description" wire:model="description" rows="3"
+                            class="
+                            block w-full rounded-md bg-white px-3 py-1.5 text-base 
+                            text-gray-900 outline-1 -outline-offset-1 outline-gray-300 
+                            placeholder:text-gray-400 focus:outline-2 
+                            focus:-outline-offset-2 focus:outline-indigo-600 
+                            sm:text-sm/6"
+                            placeholder="Descreva seu produto aqui..."></textarea>
                     </div>
                 </div>
             </div>
