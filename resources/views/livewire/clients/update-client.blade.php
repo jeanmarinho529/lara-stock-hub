3a7862f825c73a4922b987c23e537eda3c1b950f
jeanmarinho529 <jeanmarinho529@gmail.com>
2025-02-21 00:33:57 -0300

    feat: use select component

--- resources/views/livewire/clients/update-client.blade.php
+++ resources/views/livewire/clients/update-client.blade.php
@@ -22,41 +22,19 @@
                     </div>
 
                     <div class="sm:col-span-2">
-                        <label for="document_type" class="block text-sm/6 font-medium text-gray-900">Tipo do
-                            Documento*</label>
-                        <div class="mt-2 grid grid-cols-1">
-                            <select required wire:model="document_type" id="document_type" name="document_type"
-                                class="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pr-8 pl-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
-                                <option value="" disabled selected>Selecione o Tipo do Documento</option>
-                                <option value="cpf">CPF</option>
-                                <option value="cnpj">CNPJ</option>
-                            </select>
-                            <svg class="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4"
-                                viewBox="0 0 16 16" fill="currentColor" aria-hidden="true" data-slot="icon">
-                                <path fill-rule="evenodd"
-                                    d="M4.22 6.22a.75.75 0 0 1 1.06 0L8 8.94l2.72-2.72a.75.75 0 1 1 1.06 1.06l-3.25 3.25a.75.75 0 0 1-1.06 0L4.22 7.28a.75.75 0 0 1 0-1.06Z"
-                                    clip-rule="evenodd" />
-                            </svg>
-                        </div>
+                        <x-select is_required wire:model="document_type" name="document_type" label="Tipo do Documento">
+                            <option value="" disabled selected>Selecione o Tipo do Documento</option>
+                            <option value="cpf">CPF</option>
+                            <option value="cnpj">CNPJ</option>
+                        </x-select>
                     </div>
 
-
                     <div class="sm:col-span-2">
-                        <label for="type" class="block text-sm/6 font-medium text-gray-900">Tipo*</label>
-                        <div class="mt-2 grid grid-cols-1">
-                            <select required wire:model="type" id="type" name="type"
-                                class="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pr-8 pl-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
-                                <option value="" disabled selected>Selecione o Tipo</option>
-                                <option value="client">Cliente</option>
-                                <option value="supplier">Fornecedor</option>
-                            </select>
-                            <svg class="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4"
-                                viewBox="0 0 16 16" fill="currentColor" aria-hidden="true" data-slot="icon">
-                                <path fill-rule="evenodd"
-                                    d="M4.22 6.22a.75.75 0 0 1 1.06 0L8 8.94l2.72-2.72a.75.75 0 1 1 1.06 1.06l-3.25 3.25a.75.75 0 0 1-1.06 0L4.22 7.28a.75.75 0 0 1 0-1.06Z"
-                                    clip-rule="evenodd" />
-                            </svg>
-                        </div>
+                        <x-select is_required wire:model="type" name="type" label="Tipo">
+                            <option value="" disabled selected>Selecione o Tipo</option>
+                            <option value="client">Cliente</option>
+                            <option value="supplier">Fornecedor</option>
+                        </x-select>
                     </div>
 
                     <div class="sm:col-span-2">
@@ -65,23 +43,12 @@
                     </div>
 
                     <div class="sm:col-span-2">
-                        <label for="cell_number_is_whatsapp" class="block text-sm/6 font-medium text-gray-900">Celular é
-                            Whatsapp*</label>
-                        <div class="mt-2 grid grid-cols-1">
-                            <select is_required wire:model="cell_number_is_whatsapp" id="cell_number_is_whatsapp"
-                                name="cell_number_is_whatsapp"
-                                class="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pr-8 pl-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
-                                <option value="" disabled selected>Celular é Whatsapp</option>
-                                <option value="1">Sim</option>
-                                <option value="0">Não</option>
-                            </select>
-                            <svg class="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4"
-                                viewBox="0 0 16 16" fill="currentColor" aria-hidden="true" data-slot="icon">
-                                <path fill-rule="evenodd"
-                                    d="M4.22 6.22a.75.75 0 0 1 1.06 0L8 8.94l2.72-2.72a.75.75 0 1 1 1.06 1.06l-3.25 3.25a.75.75 0 0 1-1.06 0L4.22 7.28a.75.75 0 0 1 0-1.06Z"
-                                    clip-rule="evenodd" />
-                            </svg>
-                        </div>
+                        <x-select is_required wire:model="cell_number_is_whatsapp" name="cell_number_is_whatsapp"
+                            label="Celular é Whatsapp">
+                            <option value="" disabled selected>Celular é Whatsapp</option>
+                            <option value="1">Sim</option>
+                            <option value="0">Não</option>
+                        </x-select>
                     </div>
 
                     <div class="sm:col-span-2">
@@ -125,4 +92,3 @@
     </script>
 
 </div>
-
--- resources/views/livewire/orders/create-order.blade.php
+++ resources/views/livewire/orders/create-order.blade.php
@@ -3,8 +3,9 @@
 
 <div>
 
-    <div class="space-y-12">
+    <x-alert></x-alert>
 
+    <div class="space-y-12">
         <div class="border-b border-gray-900/10 pb-12">
             <h2 class="text-base/7 font-semibold text-gray-900">Dados do Cliente</h2>
             <p class="text-sm/6 text-gray-600">Dados do cliente para verificação.</p>
@@ -19,11 +20,9 @@
                 </div>
 
                 <div class="sm:col-span-2">
-                    <x-input name="client_cell_number" wire:model="client_cell_number"
-                        disabled>Celular</x-input>
+                    <x-input name="client_cell_number" wire:model="client_cell_number" disabled>Celular</x-input>
                 </div>
             </div>
-
         </div>
     </div>
 
@@ -91,8 +90,8 @@
                             </thead>
                             <tbody class="bg-white">
                                 @foreach ($selectedProducts as $item)
-                                <tr class="border-b" wire:key="product-{{ $item['code'] }}">
-                                    <td class="px-4 py-2 text-sm text-gray-700">{{ $item['code'] }}</td>
+                                    <tr class="border-b" wire:key="product-{{ $item['code'] }}">
+                                        <td class="px-4 py-2 text-sm text-gray-700">{{ $item['code'] }}</td>
                                         <td class="px-4 py-2 text-sm text-gray-700">{{ $item['brand']['name'] ?? '' }} -
                                             {{ $item['name'] }}</td>
                                         <td class="px-4 py-2 text-sm text-gray-700">R$ {{ $item['amount'] }}</td>
@@ -139,45 +138,24 @@
 
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                     <div class="sm:col-span-2">
-                        <label for="payment_method" class="block text-sm/6 font-medium text-gray-900">Método de
-                            Pagamento</label>
-                        <div class="mt-2 grid grid-cols-1">
-                            <select required wire:model="payment_method" id="payment_method" name="payment_method"
-                                class="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pr-8 pl-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
-                                <option value="" disabled selected>Escolha o Método de Pagamento</option>
-                                <option value="cash">Dinheiro</option>
-                                <option value="pix">Pix</option>
-                                <option value="credit_card">Cartão de Crédito</option>
-                                <option value="bank_slip">Boleto</option>
-                                <option value="bank_transfer">Transferência Bancária</option>
-                            </select>
-                            <svg class="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4"
-                                viewBox="0 0 16 16" fill="currentColor" aria-hidden="true" data-slot="icon">
-                                <path fill-rule="evenodd"
-                                    d="M4.22 6.22a.75.75 0 0 1 1.06 0L8 8.94l2.72-2.72a.75.75 0 1 1 1.06 1.06l-3.25 3.25a.75.75 0 0 1-1.06 0L4.22 7.28a.75.75 0 0 1 0-1.06Z"
-                                    clip-rule="evenodd" />
-                            </svg>
-                        </div>
+                        <x-select wire:model="payment_method" name="payment_method" label="Método de Pagamento"
+                            is_required>
+                            <option value="" disabled selected>Escolha o Método de Pagamento</option>
+                            <option value="cash">Dinheiro</option>
+                            <option value="pix">Pix</option>
+                            <option value="credit_card">Cartão de Crédito</option>
+                            <option value="bank_slip">Boleto</option>
+                            <option value="bank_transfer">Transferência Bancária</option>
+                        </x-select>
                     </div>
 
                     <div class="sm:col-span-1">
-                        <label for="installments" class="block text-sm/6 font-medium text-gray-900">N° de
-                            Parcelas</label>
-                        <div class="mt-2 grid grid-cols-1">
-                            <select wire:model="installments" id="installments" name="installments"
-                                class="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pr-8 pl-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
-                                <option value="1" selected>1x</option>
-                                @foreach (range(2, 12) as $number)
-                                    <option value="{{ $number }}">{{ $number }}x</option>
-                                @endforeach
-                            </select>
-                            <svg class="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4"
-                                viewBox="0 0 16 16" fill="currentColor" aria-hidden="true" data-slot="icon">
-                                <path fill-rule="evenodd"
-                                    d="M4.22 6.22a.75.75 0 0 1 1.06 0L8 8.94l2.72-2.72a.75.75 0 1 1 1.06 1.06l-3.25 3.25a.75.75 0 0 1-1.06 0L4.22 7.28a.75.75 0 0 1 0-1.06Z"
-                                    clip-rule="evenodd" />
-                            </svg>
-                        </div>
+                        <x-select wire:model="installments" name="installments" label="N° de Parcelas" is_required>
+                            <option value="1" selected>1x</option>
+                            @foreach (range(2, 12) as $number)
+                                <option value="{{ $number }}">{{ $number }}x</option>
+                            @endforeach
+                        </x-select>
                     </div>
 
                     <div class="sm:col-span-1">
@@ -186,22 +164,11 @@
                     </div>
 
                     <div class="sm:col-span-1">
-                        <label for="discount_type" class="block text-sm/6 font-medium text-gray-900">Tipo do
-                            Desconto</label>
-                        <div class="mt-2 grid grid-cols-1">
-                            <select wire:model="discount_type" @change="$wire.calculateAmountReceived()"
-                                id="discount_type" name="discount_type"
-                                class="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pr-8 pl-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
-                                <option value="percentage" selected>Porcentagem</option>
-                                <option value="absolute_value">Valor Absoluto</option>
-                            </select>
-                            <svg class="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4"
-                                viewBox="0 0 16 16" fill="currentColor" aria-hidden="true" data-slot="icon">
-                                <path fill-rule="evenodd"
-                                    d="M4.22 6.22a.75.75 0 0 1 1.06 0L8 8.94l2.72-2.72a.75.75 0 1 1 1.06 1.06l-3.25 3.25a.75.75 0 0 1-1.06 0L4.22 7.28a.75.75 0 0 1 0-1.06Z"
-                                    clip-rule="evenodd" />
-                            </svg>
-                        </div>
+                        <x-select wire:model="discount_type" name="discount_type" label="Tipo do Desconto"
+                            @change="$wire.calculateAmountReceived()">
+                            <option value="percentage" selected>Porcentagem</option>
+                            <option value="absolute_value">Valor Absoluto</option>
+                        </x-select>
                     </div>
 
                     <div class="sm:col-span-1">
@@ -213,43 +180,10 @@
             </div>
         </div>
 
-        @if (session()->has('waring'))
-            <div class="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4" role="alert">
-                <p class="font-bold">Alerta</p>
-                <p>{{ session('waring') }}</p>
-            </div>
-        @endif
-
-        @if (session()->has('success'))
-            <div class="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md"
-                role="alert">
-                <div class="flex">
-                    <div class="py-1"><svg class="fill-current h-6 w-6 text-teal-500 mr-4"
-                            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                            <path
-                                d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z" />
-                        </svg></div>
-                    <div>
-                        <p class="font-bold">Sucesso</p>
-                        <p class="text-sm">{{ session('success') }}</p>
-                    </div>
-                </div>
-            </div>
-        @endif
-
-        @if (session()->has('error'))
-            <div role="alert">
-                <div class="bg-red-500 text-white font-bold rounded-t px-4 py-2">
-                    Erro
-                </div>
-                <div class="border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700">
-                    <p>{{ session('error') }}</p>
-                </div>
-            </div>
-        @endif
+        <x-alert></x-alert>
 
         <div class="mt-6 flex items-center justify-end gap-x-6">
-            <button type="button" class="text-sm/6 font-semibold text-gray-900">Cancelar</button>
+            <a href="{{ route('orders.index') }}" class="text-sm font-semibold text-gray-900">Voltar</a>
             <button type="submit"
                 class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                 Vender
